476839780fbb2cb3cfa3d3a002a112a4bb16a287
harsha <karlakunta.sriharsha@gmail.com>
2025-02-11 13:22:28 +0530

    Implementing RESTful API endpoints using @GetMapping and @PostMapping with @RequestBody for request payload handling and @PathVariable for dynamic URL parameter extraction.

--- FirstSpring/src/main/java/com/Practice/FirstSpring/HelloController.java
+++ FirstSpring/src/main/java/com/Practice/FirstSpring/HelloController.java
@@ -1,13 +1,22 @@
 package com.Practice.FirstSpring;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello World!";
+    public HelloResponse sayHello() {
+        return new HelloResponse("Hello Spring Boot!");
+    }
+
+    @GetMapping("/hello/{name}")
+    public HelloResponse sayHelloParam(@PathVariable String name) {
+        return new HelloResponse("Hello "+name);
+    }
+
+    @PostMapping("/hello")
+    public HelloResponse sayHelloPost(@RequestBody String name) {
+        return new HelloResponse("Hello "+name+" !");
     }
 }
